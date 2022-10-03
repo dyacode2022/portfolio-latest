138d060e20ae8fb9d13f07c83a148fa9dbd43bbc
dya-code <entrydia7691@gmail.com>
2022-10-03 21:39:06 +0900

    edit navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,16 +4,19 @@
 </script>
 
 <template>
-  <nav class="navbar font-mono flex items-center m-20 font-bold justify-between -mb-40">
+  <nav class="navbar font-mono flex items-center m-20 font-bold absolute">
+    <!-- justify-between -mb-40 -->
     <img class="nav-logo w-20" src="../assets/logo.png">
-    <a href="https://github.com/dya-code" target="_blank">
-      <img class="nav-logo w-16 rounded-full brightness-75 hover:brightness-100" src="../assets/profile.jpg" href="https://github.com/dya-code">
-    </a>
-    <!-- <a class="nav-item text-slate-300 hover:text-white ml-20" href="https://github.com/dya-code">Github</a>
-    <a class="nav-item text-slate-300 hover:text-white ml-20">Discord</a>
-    <a class="nav-item text-slate-300 hover:text-white ml-20">Blog</a> -->
+    
+    <!-- <a href="https://github.com/dya-code" target="_blank"> -->
+      <!-- <img class="nav-logo w-16 rounded-full brightness-75 hover:brightness-100" src="../assets/profile.jpg" href="https://github.com/dya-code"> -->
+    <!-- </a> -->
+    <!-- <a class="nav-item text-slate-300 hover:text-white ml-20" href="https://github.com/dya-code">Github</a> -->
+    <!-- <a class="nav-item text-slate-300 hover:text-white ml-20">Discord</a> -->
+    <!-- <a class="nav-item text-slate-300 hover:text-white ml-20">Blog</a> -->
   </nav>
 </template>
+
 
 <style scoped>
   
